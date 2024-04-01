3ff7b349fe948a774b0c0feb0daae4dfb2530004
techtutortamil <techtutortamil@gmail.com>
2024-04-01 18:05:23 +1100

    Initial commit

---
+++ src/main/java/com/tecttutortamil/org/mavenlifecycledemo/MavenLifecycleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tecttutortamil.org.mavenlifecycledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenLifecycleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenLifecycleDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecttutortamil/org/mavenlifecycledemo/MavenLifecycleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecttutortamil.org.mavenlifecycledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenLifecycleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
